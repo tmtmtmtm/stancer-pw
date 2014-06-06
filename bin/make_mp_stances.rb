bb12a4bb8498eb724d047be126a136952fbc1ec5
Tony Bowden <tony@tmtm.com>
2014-06-06 13:35:10 +0300

    handle failure in mp_stances
    group() can't handle more than 20000 unique keys, which is an issue
    sometimes

--- bin/make_mp_stances.rb
+++ bin/make_mp_stances.rb
@@ -2,19 +2,25 @@
 
 require 'json'
 require 'stancer'
+require 'parallel'
 
 issues = JSON.parse(File.read('data.json'))
 parties = JSON.parse(File.read('parties.json'))
 
 issues.each do |i|
-  parties.each do |p|
-    warn "Calculating #{i['text']} (#{i['id']}) for #{p['name']}"
-    aspect = Aspect.new(
-      bloc:'voter.id',
-      filter: "party.id:#{p['id']}",
-      issue: Issue.new(i['id']),
-    )
-    puts JSON.pretty_generate(aspect.scored_blocs)
-    # TODO merge them all
+  begin
+    stances = parties.map { |p|
+      warn "Calculating #{i['text']} (#{i['id']}) for #{p['name']}"
+      # Parallel.each(..., :in_threads => 10) do |i|
+      Aspect.new(
+        bloc:'voter.id',
+        filter: "party.id:#{p['id']}",
+        issue: Issue.new(i['id']),
+      ).scored_blocs
+    }.reduce(:merge)
+    i['stances'] = stances
+    puts JSON.pretty_generate(i)
+  rescue
+    warn "PROBLEM with #{i['text']} (#{i['id']})"
   end
 end
